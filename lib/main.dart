655f882466c0a31cc9c75ac42ba7efdf0dd16228
Ryusei-0407 <ryu000407@gmail.com>
2021-06-25 00:44:53 +0900

    add Form and Login Button

--- lib/main.dart
+++ lib/main.dart
@@ -32,14 +32,73 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          children: <Widget>[
-            new Image.asset(
-              'images/tech.png',
-              width: 400.0,
-              height: 400.0,
-            ),
-          ],
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: <Widget>[
+              new Image.asset(
+                'images/tech.png',
+                width: 400.0,
+                height: 400.0,
+              ),
+              // Name form
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: Icon(
+                    Icons.person,
+                    color: Colors.black,
+                  ),
+                  labelText: 'Name',
+                ),
+                onSaved: (String? value) {},
+                validator: (String? value) {
+                  return (value == null || value.isEmpty) ? 'No input' : null;
+                },
+              ),
+              // Mail address form
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: Icon(
+                    Icons.mail,
+                    color: Colors.black,
+                  ),
+                  labelText: 'Mail address',
+                ),
+                onSaved: (String? value) {},
+                validator: (String? value) {
+                  return (value == null || value.isEmpty) ? 'No input' : null;
+                },
+              ),
+              // Password form
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: Icon(
+                    Icons.https,
+                    color: Colors.black,
+                  ),
+                  labelText: 'Password',
+                ),
+                onSaved: (String? value) {},
+                validator: (String? value) {
+                  return (value == null || value.isEmpty) ? 'No input' : null;
+                },
+              ),
+              Container(
+                padding: const EdgeInsets.all(13.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.black,
+                    minimumSize: Size(300.0, 40.0),
+                  ),
+                  onPressed: () {
+                    // TODO
+                  },
+                  child: Text('Login'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
